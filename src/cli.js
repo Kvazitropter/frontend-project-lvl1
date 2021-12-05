807aa33f97fceaed3257f7941653abf28447ebaf
Tanya Kachulina <kachulinatanya@yandex.ru>
2021-12-05 15:37:32 +0300

    fix syntax errors

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
+
 export default () => {
-  const name = readlineSync.question("May I have your name? ").trim();
+  const name = readlineSync.question('May I have your name? ').trim();
   console.log(`Hello, ${name}!`);
 };
-
-
