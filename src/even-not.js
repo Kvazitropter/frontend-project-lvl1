8aac9deaf6980f5929bd3e95a8c8bb3c6468882b
Tanya Kachulina <kachulinatanya@yandex.ru>
2021-12-08 12:20:57 +0300

    upgrade game files

--- src/even-not.js
+++ src/even-not.js
@@ -1,37 +1,21 @@
-import readlineSync from 'readline-sync';
+import { anyGame } from './index.js';
 
 export const evenOrNotGame = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ').trim();
-  console.log(`Hello, ${name}!`);
-
   const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
-  let workNum = 1;
-  let num = 0;
-  let isEven = '';
+  const numbers = [];
+  const answers = [];
+  let workNum = 0;
 
-  console.log(rules);
-
-  while (workNum <= 3) {
-    num = Math.ceil(Math.random() * 100);
-    console.log(`Question: ${num}`);
+  while (workNum < 3) {
+    let num = Math.ceil(Math.random() * 1000);
+    numbers.push(num);
     if (num % 2 === 0) {
-      isEven = 'yes';
+      answers.push('yes');
     } else {
-      isEven = 'no';
+      answers.push('no');
     }
-    const answer = readlineSync.question('Your answer: ').trim();
-    if (isEven === answer) {
-      console.log('Correct!');
-      workNum += 1;
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven}'.`);
-      console.log(`Let's try again, ${name}!`);
-      break;
-    }
+    workNum += 1;
   }
-  
-  if (workNum === 4) {
-    console.log(`Congratulations, ${name}!`);
-  }
+
+  anyGame(rules, numbers, answers);
 };
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import readlineSync from 'readline-sync';
+
+export const anyGame = (rules, questions, rightAnswers) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ').trim();
+  console.log(`Hello, ${name}!`);
+
+  let workNum = 0;
+
+  console.log(rules);
+
+  while (workNum < 3) {
+    console.log(`Question: ${questions[workNum]}`);
+    const answer = readlineSync.question('Your answer: ').trim();
+    if (rightAnswers[workNum] === answer) {
+      console.log('Correct!');
+      workNum += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswers[workNum]}'.`);
+      console.log(`Let's try again, ${name}!`);
+      break;
+    }
+  }
+  
+  if (workNum === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+
