128c6e710f486c6f16e5ae01e13594647bba2e9d
Tanya Kachulina <kachulinatanya@yandex.ru>
2021-12-10 11:25:47 +0300

    add brain-game is primenumber

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { isPrimeGame } from '../src/is-prime.js';
+
+isPrimeGame();
---
+++ src/is-prime.js
@@ -0,0 +1,37 @@
+import { anyGame } from './index.js';
+
+export const findDivisors = (num) => {
+  const arrayDiv = [];
+  let div = num;
+
+  while (div >= 1) {
+    if (num % div === 0) {
+      arrayDiv.push(div)
+    } 
+    div -= 1;
+  }
+
+  return arrayDiv;
+}
+
+export const isPrimeGame = () => {
+  const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const numbers = [];
+  const answers = [];
+  let workNum = 0;
+
+  while (workNum < 3) {
+    let num = Math.ceil(Math.random() * 100);
+    numbers.push(num);
+    let divCount = findDivisors(num).length;
+    if (divCount === 2) {
+      answers.push('yes');
+    } else {
+      answers.push('no');
+    }
+    workNum += 1;
+  }
+
+  anyGame(rules, numbers, answers);
+};
+
